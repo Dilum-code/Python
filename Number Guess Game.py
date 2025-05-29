4a14385d495d4a5169025c24bff6bd029e75b5eb
Dilum-code <dilumsevwandi@gmail.com>
2025-05-29 13:33:00 +0200

    Add files via upload

---
+++ Number Guess Game.py
@@ -0,0 +1,17 @@
+import random
+
+number=random.randint(1,100)
+guess=0
+
+while guess!=number:
+    guess=int(input("Enter your guess number:"))
+
+    if(guess<number):
+        print("Guess higher!")
+
+    elif(guess>number):
+        print("Guess Lower!")
+
+    else:
+        print("You won!")
+
