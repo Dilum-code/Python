24473dd177f61cde66098f91e540f1da39c08762
Dilum-code <dilumsevwandi@gmail.com>
2025-05-29 13:30:57 +0200

    Add files via upload
    Temp Converter

---
+++ TempConverter_FtoC.py
@@ -0,0 +1,21 @@
+
+print("1=F to C:")
+print("2=C to F:")
+
+option = int(input("Select a Converter from above : "))
+
+if option in [1,2]
+
+
+else:
+    print("Invalid option")
+
+value_F=int(input("Enter Temperature from F: "))
+value_C=int(input("Enter Temperature from C: "))
+
+if(value_F )
+
+valueconverted= (value * 9/5) + 32
+valueconverted=(value - 32) * 5/9
+
+print("Converted value is {}".format(valueconverted))
